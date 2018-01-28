c595eac773e7ea157e726eb756e6050a42198933
Niklas Fiekas <niklas.fiekas@backscattering.de>
2018-01-28 23:38:21 +0100

    fix LICENSE-MIT

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-// Copyright 2017 Niklas Fiekas <niklas.fiekas@backscattering.de>
+// Copyright 2017-2018 Niklas Fiekas <niklas.fiekas@backscattering.de>
 //
 // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
 // http://www.apache.org/licenses/LICENSE-2.0> or the MIT license

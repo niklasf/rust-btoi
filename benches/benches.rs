b55e8b0fe36324c76c5fcc78db69f955df797f96
Niklas Fiekas <niklas.fiekas@backscattering.de>
2025-01-01 10:41:14 +0100

    also blackbox benchmark output

--- benches/benches.rs
+++ benches/benches.rs
@@ -5,28 +5,43 @@ extern crate btoi;
 use bencher::{black_box, Bencher};
 
 fn bench_btou(b: &mut Bencher) {
-    b.iter(|| assert_eq!(Ok(123_456_789u32), btoi::btou(black_box(b"123456789"))));
+    b.iter(|| {
+        assert_eq!(
+            Ok(black_box(123_456_789u32)),
+            btoi::btou(black_box(b"123456789"))
+        )
+    });
 }
 
 fn bench_btou_saturating(b: &mut Bencher) {
     b.iter(|| {
         assert_eq!(
-            Ok(123_456_789u32),
+            Ok(black_box(123_456_789u32)),
             btoi::btou_saturating(black_box(b"123456789"))
         )
     });
 }
 
 fn bench_small_btou(b: &mut Bencher) {
-    b.iter(|| assert_eq!(Ok(42u8), btoi::btou(black_box(b"42"))));
+    b.iter(|| assert_eq!(Ok(black_box(42u8)), btoi::btou(black_box(b"42"))));
 }
 
 fn bench_small_btou_saturating(b: &mut Bencher) {
-    b.iter(|| assert_eq!(Ok(255u8), btoi::btou_saturating(black_box(b"256"))));
+    b.iter(|| {
+        assert_eq!(
+            Ok(black_box(255u8)),
+            btoi::btou_saturating(black_box(b"256"))
+        )
+    });
 }
 
 fn bench_btoi(b: &mut Bencher) {
-    b.iter(|| assert_eq!(Ok(-123_456_789i32), btoi::btoi(black_box(b"-123456789"))));
+    b.iter(|| {
+        assert_eq!(
+            Ok(black_box(-123_456_789i32)),
+            btoi::btoi(black_box(b"-123456789"))
+        )
+    });
 }
 
 fn bench_from_str(b: &mut Bencher) {
@@ -34,7 +49,12 @@ fn bench_from_str(b: &mut Bencher) {
         ::std::str::from_utf8(s).ok().and_then(|s| s.parse().ok())
     }
 
-    b.iter(|| assert_eq!(Some(123_456_789), btou_from_str(black_box(b"123456789"))));
+    b.iter(|| {
+        assert_eq!(
+            Some(black_box(123_456_789)),
+            btou_from_str(black_box(b"123456789"))
+        )
+    });
 }
 
 fn bench_from_str_unchecked(b: &mut Bencher) {
@@ -43,7 +63,7 @@ fn bench_from_str_unchecked(b: &mut Bencher) {
     }
 
     b.iter(|| {
-        assert_eq!(Some(123_456_789), unsafe {
+        assert_eq!(Some(black_box(123_456_789)), unsafe {
             btou_from_str_unchecked(black_box(b"123456789"))
         })
     });
@@ -54,7 +74,7 @@ fn bench_small_from_str(b: &mut Bencher) {
         ::std::str::from_utf8(s).ok().and_then(|s| s.parse().ok())
     }
 
-    b.iter(|| assert_eq!(Some(42u8), btou_from_str(black_box(b"42"))));
+    b.iter(|| assert_eq!(Some(black_box(42u8)), btou_from_str(black_box(b"42"))));
 }
 
 fn bench_small_from_str_unchecked(b: &mut Bencher) {
@@ -63,7 +83,7 @@ fn bench_small_from_str_unchecked(b: &mut Bencher) {
     }
 
     b.iter(|| {
-        assert_eq!(Some(42u8), unsafe {
+        assert_eq!(Some(black_box(42u8)), unsafe {
             btou_from_str_unchecked(black_box(b"42"))
         })
     });
